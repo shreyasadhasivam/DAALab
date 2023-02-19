69af82d9d4ae1fb4fd9302216e638d0c7b5aa202
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2023-02-19 21:46:38 +0530

    Update Q1.c

--- lab3/Q1.c
+++ lab3/Q1.c
@@ -33,7 +33,7 @@ int main()
 	printf("Enter the size of the array: ");
 	scanf("%d", &n);
 	int arr[n];
-	printf("\nEnter the array in the elements: ");
+	printf("\nEnter the elements in the array: ");
 	for(int i=0;i<n;i++)
 	{
 		scanf("%d", &arr[i]);
