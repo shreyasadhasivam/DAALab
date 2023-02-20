80bcdd00a375924ea8de9c74e903a4ac59496905
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2023-02-20 16:36:49 +0530

    Update matrixmul.c

--- lab4/matrixmul.c
+++ lab4/matrixmul.c
@@ -1,25 +1,49 @@
 #include<stdio.h>
-int m,n,p,q;
-void matrixmul(int A[m][n],int B[p][q],int C[m][q])
-{
-	//int m,n,p,q;
-	//int A[m][n],B[p][q],C[][];
-	int i,j,k;
-	if(n==p)
-	{
-		for( i=0;i<m;i++)
-		{
-			for( j=0;j<q;j++)
-				C[i][j]=0;
-				for( k=0;k<p;k++)
-					C[i][j] += A[i][k] * B[k][j];
-				
-		}
-	}
-}
-
+//int opcount =0;
 int main()
 {
-	
-	return 0;
+  int c, d, p, q, m, n, k, tot = 0;
+  int fst[10][10], sec[10][10], mul[10][10];
+
+  printf("Enter the rows and columns for matrix A: \n");
+  scanf("%d%d", &m, &n);
+
+  printf("Insert your matrix elements : \n");
+  for (c = 0; c < m; c++)
+    for (d = 0; d < n; d++)
+      scanf("%d", &fst[c][d]);
+ 
+  printf("Enter the rows and columns for matrix B: \n");
+  scanf(" %d %d", &p, &q);
+
+  if (n != p)
+    printf("Multiplication is not possible\n");
+  else
+  {
+    printf("Insert matrix elements \n");
+ 
+    for (c = 0; c < p; c++)
+      for (d = 0; d < q; d++)
+        scanf("%d", &sec[c][d] );
+
+    for (c = 0; c < m; c++) {
+      for (d = 0; d < q; d++) {
+        for (k = 0; k < p; k++) 
+        {
+        	//opcount++
+          	tot = tot + fst[c][k] * sec[k][d];
+        }
+        mul[c][d] = tot;
+        tot = 0;
+      }
+    }
+ 
+    printf("The result of matrix multiplication: \n"); 
+    for (c = 0; c < m; c++) {
+      for (d = 0; d < q; d++)
+        printf("%d \t", mul[c][d] );
+      printf(" \n ");
+    }
+  }
+  return 0;
 }
